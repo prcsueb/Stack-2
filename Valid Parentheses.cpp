8e2b297bbf3c99ba177aa6854a74a8a3f5b20a49
prcsueb <premang.raithatha.1@gmail.com>
2024-10-03 14:44:28 -0700

    Completed Stack-2

---
+++ Valid Parentheses.cpp
@@ -0,0 +1,29 @@
+//time: O(n)
+//space: O(n)
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for(char c : s) {
+            if(c == '(') {
+                stk.push(')');
+            }
+            else if(c == '{') {
+                stk.push('}');
+            }
+            else if(c == '[') {
+                stk.push(']');
+            }
+            else {
+                if(stk.empty() || c != stk.top()) {
+                    return false;
+                }
+                stk.pop();
+            }
+        }
+        if(!stk.empty()) {
+            return false;
+        }
+        return true;
+    }
+};
